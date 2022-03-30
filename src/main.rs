91abb8ce322504c26ca17391df4174aaf620c08b
Daniel Saxton <dsaxton@pm.me>
2022-03-30 12:36:45 -0500

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = "Minimal symmetric encryption utility")]
+struct Args {
+    /// Encryption key
+    #[clap(short, long)]
+    key: String,
+    /// File name
+    #[clap(short, long)]
+    file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    println!("Key: {}", args.key);
+    println!("File: {}", args.file);
+
+    match std::fs::read(args.file) {
+        Ok(bytes) => {
+            println!("{:?}", bytes);
+        }
+        Err(e) => {
+            panic!("{}", e)
+        }
+    }
 }
