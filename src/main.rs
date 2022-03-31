9a5f5b9c1c07af0e2275cf30921b04eb87a7df7e
Daniel Saxton <dsaxton@pm.me>
2022-03-31 09:49:39 -0500

    Some names

--- src/main.rs
+++ src/main.rs
@@ -21,23 +21,23 @@ fn main() {
     let mut hasher = Sha512::new();
     hasher.update(args.key.as_bytes());
     let hashed_key: Vec<u8> = hasher.finalize().to_vec();
-    let file_bytes = match std::fs::read(args.input) {
+    let input_file_bytes = match std::fs::read(args.input) {
         Ok(bytes) => bytes,
         Err(e) => {
             panic!("{}", e);
         } 
     };
 
-    let path = std::path::Path::new(&args.output);
-    let mut file = match std::fs::File::create(path) {
+    let output_path = std::path::Path::new(&args.output);
+    let mut output_file = match std::fs::File::create(output_path) {
         Ok(f) => f,
         Err(e) => {
             panic!("{}", e);
         }
     };
 
-    for (idx, file_byte) in file_bytes.iter().enumerate() {
+    for (idx, file_byte) in input_file_bytes.iter().enumerate() {
         let xor_byte = file_byte ^ hashed_key[idx % hashed_key.len()];
-        file.write_all(&[xor_byte]).expect("Unable to write byte");
+        output_file.write_all(&[xor_byte]).expect("Unable to write byte");
     }
 }
